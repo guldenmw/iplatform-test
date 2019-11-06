111e9c7d9477c5d5baab66f09fcd5044e78b7fb2
guldenmw <mw@custostech.com>
2019-11-06 20:50:21 +0200

    - removed redux-persist to avoid potential caching problems during demos and reviews

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -12,7 +12,7 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar} from 'react-bootstrap';
 
-import { store, persistor } from './store';
+import { store } from './store';
 
 import SearchArtists from '../pages/SearchArtists';
 import SearchReleases from '../pages/SearchReleases';
@@ -22,30 +22,28 @@ import Favorites from '../pages/Favorites';
 const App: FC = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <Navbar bg='primary'>
-            <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-            <Navbar.Collapse>
-              <Link className={'text-white font-weight-bold mx-2 my-2'} to='/my_artists'>My Artists</Link>
-              <Link className={'text-white font-weight-bold mx-2 my-2'} to='/search_artists'>Search Artists</Link>
-              <Link className={'text-white font-weight-bold mx-2 my-2'} to='/search_releases'>Search Releases</Link>
-            </Navbar.Collapse>
-          </Navbar>
+      <Router>
+        <Navbar bg='primary'>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+          <Navbar.Collapse>
+            <Link className={'text-white font-weight-bold mx-2 my-2'} to='/my_artists'>My Artists</Link>
+            <Link className={'text-white font-weight-bold mx-2 my-2'} to='/search_artists'>Search Artists</Link>
+            <Link className={'text-white font-weight-bold mx-2 my-2'} to='/search_releases'>Search Releases</Link>
+          </Navbar.Collapse>
+        </Navbar>
 
-          <Switch>
-            <Route path='/my_artists'>
-                <Favorites/>
-            </Route>
-            <Route path='/search_artists'>
-                <SearchArtists/>
-            </Route>
-            <Route path='/search_releases'>
-                <SearchReleases/>
-            </Route>
-          </Switch>
-        </Router>
-      </PersistGate>
+        <Switch>
+          <Route path='/my_artists'>
+              <Favorites/>
+          </Route>
+          <Route path='/search_artists'>
+              <SearchArtists/>
+          </Route>
+          <Route path='/search_releases'>
+              <SearchReleases/>
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   );
 };
--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,6 +1,4 @@
 import {combineReducers, createStore, applyMiddleware} from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import favorites from "./favorites/reducer";
 import musicBrainzReducer from "./search/musicbrainz/reducer";
 import lastFMReducer from "./search/lastfm/reducer";
@@ -8,10 +6,6 @@ import shortlist from "./shortlist/reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from 'redux-thunk';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
 
 const rootReducer = combineReducers({
   favorites,
@@ -20,16 +14,14 @@ const rootReducer = combineReducers({
   shortlist,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export type AppState = ReturnType<typeof rootReducer>;
 
-export const store = createStore(
-  persistedReducer,
+const store = createStore(
+  rootReducer,
   composeWithDevTools(
     applyMiddleware(thunk)
   )
 );
 
-export const persistor = persistStore(store);
-
+export default store;
